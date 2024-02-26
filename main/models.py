604b854b2eefa2882ed1d463371de40c5f53d170
Reis567 <matheusdosreis9@gmail.com>
2024-02-26 18:37:31 -0300

    Create the Mopdel of product

--- main/models.py
+++ main/models.py
@@ -12,3 +12,12 @@ class ProductCategory(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Product(models.Model):
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    title = models.CharField(max_length=255, unique=True)
+    detail = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} - ${self.price}"
