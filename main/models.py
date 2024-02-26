7fdeed0d2af4a1f289ded653249220b2197e2dbc
Reis567 <matheusdosreis9@gmail.com>
2024-02-26 18:34:56 -0300

    Create the model of ProductCaterogy

--- main/models.py
+++ main/models.py
@@ -5,3 +5,10 @@ from django.contrib.auth.models import User
 class Vendor(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     adress = models.TextField(null=True)
+
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=255)
+    detail = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
