a43b10fc9865b0a14bd488490b82f66d38a11050
Reis567 <matheusdosreis9@gmail.com>
2024-02-26 18:27:09 -0300

    Initial setup of Vendor model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+# Vendor models
 
-# Create your models here.
+class Vendor(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    adress = models.TextField(null=True)
