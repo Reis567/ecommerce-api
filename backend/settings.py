422ae22643364b7b94f14391912b604fb5fd49ac
Reis567 <matheusdosreis9@gmail.com>
2024-02-27 07:55:27 -0300

    Add drf-spectacular and the documentation setup

--- backend/settings.py
+++ backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'main',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK={
+    'DEFAULT_SCHEMA_CLASS':'drf_spectacular.openapi.AutoSchema'
+}
+
+SPECTACULAR_SETTINGS={
+    'TITLE':'Django DRF Ecommerce',
+}
