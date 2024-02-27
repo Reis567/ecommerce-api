6dc8310e1adefb4b9fd6d9bcc367097cb4bce08c
Reis567 <matheusdosreis9@gmail.com>
2024-02-27 07:56:00 -0300

    Url link to download the schem

--- backend/urls.py
+++ backend/urls.py
@@ -21,4 +21,5 @@ from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('main.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
 ]
