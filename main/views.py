2f13347842f90b32d7285606796a56b162d00815
Reis567 <matheusdosreis9@gmail.com>
2024-02-27 07:23:12 -0300

    View to list Vendors

--- main/views.py
+++ main/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
+from .serializers import *
+from rest_framework import generics
+from .models import *
 
+
+class VendorList(generics.ListAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
