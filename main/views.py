5a103b8c3467e0dd39ebc1c2d29f2eb1cfb103ff
Reis567 <matheusdosreis9@gmail.com>
2024-02-28 18:40:06 -0300

    Vendor list view needs authentication

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .serializers import *
-from rest_framework import generics
+from rest_framework import generics,permissions
 from .models import *
 
 
 class VendorList(generics.ListAPIView):
     queryset = Vendor.objects.all()
     serializer_class = VendorSerializer
+    permission_classes=[permissions.IsAuthenticated]
