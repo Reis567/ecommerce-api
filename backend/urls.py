a010d64481856d29058c83d0d59727585b7e0197
Reis567 <matheusdosreis9@gmail.com>
2024-02-29 17:09:11 -0300

    User register endpoint prefix

--- backend/urls.py
+++ backend/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), 
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('user/register/', views.UserRegistrationView.as_view(), name='user-registration'),
+    path('api/user/register/', views.UserRegistrationView.as_view(), name='user-registration'),
 ]
