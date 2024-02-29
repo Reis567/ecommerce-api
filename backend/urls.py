46810757674a7b8603ca998edb5f93e782851ed3
Reis567 <matheusdosreis9@gmail.com>
2024-02-29 17:08:48 -0300

    User register endpoint passed to base project urls

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path,include
 from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from ..main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +11,6 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), 
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/register/', views.UserRegistrationView.as_view(), name='user-registration'),
 ]
--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,4 @@ from .views import *
 urlpatterns = [
     path('vendors/', VendorList.as_view(), name='vendor-list'),
     path('vendor/', VendorDetail.as_view(), name='vendor-detail'),
-     path('user/register/', UserRegistrationView.as_view(), name='user-registration'),
 ]
