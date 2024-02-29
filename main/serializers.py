6de61bbfcbd4b0cedd66ee8139dbba0ba6e07654
Reis567 <matheusdosreis9@gmail.com>
2024-02-29 08:15:04 -0300

    User serializer

--- main/serializers.py
+++ main/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth.models import User
 
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model=Vendor
         fields=['user','address']
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
