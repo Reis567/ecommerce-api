f59aa08d953eb40c6f01cd975fafec8e4f0ced07
Reis567 <matheusdosreis9@gmail.com>
2024-03-24 16:29:24 -0300

    Feat : URL of update

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('vendors/', VendorListView.as_view(), name='vendor-list'),
     path('vendor/', VendorRetrieveView.as_view(), name='vendor-detail'),
     path('vendors/create/', VendorCreateView.as_view(), name='vendor-create'),
+    path('vendors/<int:pk>/update/', VendorUpdateView.as_view(), name='vendor-update'),
 ]
