0417a3950196f247ba4fe17329d302c6d6bfaebd
Reis567 <matheusdosreis9@gmail.com>
2024-03-29 19:35:22 -0300

    Feat : urls of product and product category

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('vendor/create/', VendorCreateView.as_view(), name='vendor-create'),
     path('vendor/<int:pk>/update/', VendorUpdateView.as_view(), name='vendor-update'),
     path('vendor/<int:pk>/delete/', VendorDestroyView.as_view(), name='vendor-delete'),
+
+    path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('categories/', ProductCategoryListView.as_view(), name='category-list'),
+    path('categories/<int:pk>/', ProductCategoryDetailView.as_view(), name='category-detail'),
 ]
