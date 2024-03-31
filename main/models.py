b2ca4eee106a6be9739390ebaca73ac2053355ed
Reis567 <matheusdosreis9@gmail.com>
2024-03-31 10:53:56 -0300

    Feat : Migrations of vendor fk

--- main/models.py
+++ main/models.py
@@ -19,6 +19,7 @@ class ProductCategory(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL,null=True)
+    vendor = models.ForeignKey(Vendor,on_delete=models.SET_NULL,null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     title = models.CharField(max_length=255, unique=True)
     detail = models.TextField(blank=True, null=True)
