82f734158aade75c7d8a22a058f860dcb318b1d8
Reis567 <matheusdosreis9@gmail.com>
2024-04-05 22:57:10 -0300

    Feat : Auditoria

--- main/models.py
+++ main/models.py
@@ -25,11 +25,14 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL,null=True)
-    vendor = models.ForeignKey(Vendor,on_delete=models.SET_NULL,null=True)
+    category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL, null=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     title = models.CharField(max_length=255, unique=True)
     detail = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title} - ${self.price}"
