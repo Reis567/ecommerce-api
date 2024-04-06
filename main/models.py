4df560d6bb36d13b68eb3aae667b103358547d57
Reis567 <matheusdosreis9@gmail.com>
2024-04-05 22:57:57 -0300

    Feat : Auditoria

--- main/models.py
+++ main/models.py
@@ -58,8 +58,11 @@ class Order(models.Model):
         return str(self.order_time)
 
 class OrderItems(models.Model):
-    order = models.ForeignKey(Order,on_delete=models.CASCADE)
-    product = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.product.title
