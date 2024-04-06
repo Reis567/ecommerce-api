9fc57245d653966175631df24cd005d25f4e1c2a
Reis567 <matheusdosreis9@gmail.com>
2024-04-06 09:51:24 -0300

    Feat : Condition field in product

--- main/models.py
+++ main/models.py
@@ -24,9 +24,23 @@ class ProductCategory(models.Model):
     def __str__(self):
         return self.title
 
+class ProductCondition(models.Model):
+    CONDITION_CHOICES = [
+        ('new', 'Novo'),
+        ('used', 'Usado'),
+        ('refurbished', 'Recondicionado'),
+    ]
+
+    condition = models.CharField(max_length=20, choices=CONDITION_CHOICES)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.get_condition_display()
 
 class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL, null=True)
+    condition=models.ForeignKey(ProductCondition, on_delete=models.SET_NULL, null=True)
     vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     title = models.CharField(max_length=255, unique=True)
