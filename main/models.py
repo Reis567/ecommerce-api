1767987a45bc1572e9117c6ce389c8e46d4ad83d
Reis567 <matheusdosreis9@gmail.com>
2024-04-05 22:56:43 -0300

    Feat : Auditoria

--- main/models.py
+++ main/models.py
@@ -17,6 +17,9 @@ class ProductCategory(models.Model):
     title = models.CharField(max_length=255)
     detail = models.TextField(blank=True, null=True)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
 
