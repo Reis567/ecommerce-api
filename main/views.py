0cdde8831e0791e916bf8b325e4d888ba11d5a85
Reis567 <matheusdosreis9@gmail.com>
2024-04-10 17:27:39 -0300

    Feat : CustomerAddressViewSet

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics,permissions
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.viewsets import *
 
 from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
 
@@ -283,3 +284,8 @@ class OrderDeleteView(generics.DestroyAPIView):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class CustomerAddressViewSet(ModelViewSet):
+    serializer_class = CustomerAddressSerializer
+    queryset = CustomerAddress.objects.all()
+    
