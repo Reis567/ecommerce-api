5db77b599c7dc3a9bfd3c46b7f69c8cd13f5e271
Reis567 <matheusdosreis9@gmail.com>
2024-04-12 13:49:32 -0300

    Fix : customer field name fix

--- main/models.py
+++ main/models.py
@@ -83,7 +83,7 @@ class OrderItems(models.Model):
         return self.product.title
     
 class CustomerAddress(models.Model):
-    Customer = models.ForeignKey(Customer, on_delete=models.CASCADE,related_name='customer_addresses')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE,related_name='customer_addresses')
     address=models.TextField()
     default_address = models.BooleanField(default=False)
 
