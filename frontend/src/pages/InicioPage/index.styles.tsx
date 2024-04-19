c58eef34bcf1ae5b15a1c658fbd8eb86a5c53d87
Reis567 <matheusdosreis9@gmail.com>
2024-04-19 08:42:37 -0300

    Feat : Home styled div and title

---
+++ frontend/src/pages/InicioPage/index.styles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+    display: flex;
+    width: 100%;
+    min-height: 100vh;
+    text-align: center;
+
+`
+
+
+export const InicioTitle = styled.h3`
+    font-size: 35px;
+`
--- frontend/src/pages/InicioPage/index.tsx
+++ frontend/src/pages/InicioPage/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import{ Card }from"antd";
 const { Meta } = Card;
+import {Container,InicioTitle}from './index.styles.tsx';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 const Inicio: React.FC = () => {
   return (
-    <div>
-  <Card
-    hoverable
-    style={{ width: 240 }}
-    cover={<img alt="example" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl9su24gdGNlP0Dh4CtT5G4f8KsijhjET9cOymhV8XKg&s" />}
-  >
-    <Meta title="Europe Street beat" description="www.instagram.com" />
-  </Card>
+    <Container>
+      <InicioTitle>
+        Produtos
+      </InicioTitle>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt="example" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl9su24gdGNlP0Dh4CtT5G4f8KsijhjET9cOymhV8XKg&s" />}
+      >
+        <Meta title="Europe Street beat" description="www.instagram.com" />
+      </Card>
 
-    </div>
+    </Container>
   )
 }
 
