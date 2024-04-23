58c74812b95122bda5533db07fd12aa511c09ad2
Reis567 <matheusdosreis9@gmail.com>
2024-04-22 22:40:19 -0300

    Feat : Dropdown of categories

--- frontend/src/components/Header/Header.styles.tsx
+++ frontend/src/components/Header/Header.styles.tsx
@@ -14,6 +14,7 @@ export const Container = styled.div`
   border-bottom-left-radius: 40px;
   border-bottom-right-radius: 40px;
   
+  
 
   &.responsive_nav {
     transform:none;
@@ -21,6 +22,8 @@ export const Container = styled.div`
 `;
 
 export const Nav = styled.nav<{isOpen:boolean}>`
+    position: relative;
+
 
 
     @media only screen and  (max-width: 1024px) {
@@ -66,6 +69,57 @@ export const SLink = styled(Link)`
     width: 100%;
   }
 `;
+export const CategoItem = styled(Link)`
+  margin: 5px;
+  padding: 5px;
+  color: #faa757;
+  font-weight: bolder;
+  font-size: 20px;
+  position: relative;
+  text-decoration:None;
+
+  &::after {
+    position: absolute;
+    content: " ";
+    width: 0;
+    height: 2px;
+    background: #faa757;
+    bottom: -20%;
+    left: 0;
+    transition: width 0.3s ease-in-out; 
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+`;
+
+
+export const CategoLink = styled.span`
+  font-weight: bolder;
+  color: #faa757;
+  font-weight: bolder;
+  font-size: 20px;
+  position: relative;
+
+
+  cursor: pointer;
+  &::after {
+    position: absolute;
+    content: " ";
+    width: 0;
+    height: 1px;
+    background: #faa757;
+    bottom: 0;
+    left: 0;
+    transition: width 0.3s ease-in-out; 
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+`;
+
 
 export const NavTitle = styled.h3`
   font-size: 30px;
@@ -79,3 +133,22 @@ export const NavTitle = styled.h3`
     font-size: 20px;
   }
 `
+
+export const CategoryList = styled.div`
+  gap: 10px;
+    position: absolute;
+    padding-bottom: 15px;
+    left: 15%;
+    width: 26%;
+    top: 180%;
+    background-color: #ffffff;
+    border: 1px solid #ccc;
+    border-top: none; /* Remove a borda superior para que não se sobreponha ao link de categorias */
+    border-radius: 0 0 5px 5px; /* Arredonda apenas os cantos inferiores */
+    z-index: 1; /* Defina um z-index menor para que a lista não se sobreponha a outros elementos */
+    display: flex; /* Exibe os itens em uma coluna */
+    flex-direction: column; /* Organiza os itens em uma coluna */
+
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1); /* Adiciona uma sombra leve */
+
+`;
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,48 +1,75 @@
-import React, { useState } from 'react';
-import {FaBars, FaTimes} from 'react-icons/fa'
-import { Container,Nav,NavTitle,SLink } from './Header.styles.tsx';
+import React, { useState, useEffect, useRef } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Container, Nav, NavTitle, SLink, CategoryList, CategoLink, CategoItem } from './Header.styles.tsx';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const Header: React.FC = () => {
     const [isNavOpen, setIsNavOpen] = useState(false);
+    const [isCategoryOpen, setIsCategoryOpen] = useState(false);
+    const categoryRef = useRef<HTMLSpanElement>(null);
 
     const toggleNavBar = () => {
-      setIsNavOpen(!isNavOpen);
+        setIsNavOpen(!isNavOpen);
     };
 
+    const toggleCategoryList = () => {
+        setIsCategoryOpen(!isCategoryOpen);
+    };
 
-  return (
-    <Container>
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (categoryRef.current && !categoryRef.current.contains(event.target as Node)) {
+                setIsCategoryOpen(false);
+            }
+        }
 
-    <NavTitle>
-      <Link to={"/"}>
-       E-Commerce
-      </Link>
-    </NavTitle>
-    
-    <Nav isOpen={isNavOpen} as="div">
-        <SLink to="/" onClick={toggleNavBar}>Inicio</SLink>
-        <SLink to="/sobre" onClick={toggleNavBar}>Categorias</SLink>
-        <SLink to="/experiencia" onClick={toggleNavBar}>
-          Carrinho
-        <FontAwesomeIcon icon={faCartShopping} />
-        </SLink>
-        <SLink to="/projetos" onClick={toggleNavBar}>Projetos</SLink>
-        <SLink to="/contatos" onClick={toggleNavBar}>Contatos</SLink>
-        <button className='nav-btn nav-close-btn' onClick={toggleNavBar}>
-            <FaTimes/>
-        </button>
-    </Nav>
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
-    <button className='nav-btn' onClick={toggleNavBar}>
-            <FaBars/>
-    </button>
+    return (
+        <Container>
 
-    </Container>
-  );
+            <NavTitle>
+                <Link to={"/"}>
+                    E-Commerce
+                </Link>
+            </NavTitle>
+
+            <Nav isOpen={isNavOpen} as="div">
+                <SLink to="/" onClick={toggleNavBar}>Inicio</SLink>
+                <CategoLink onClick={toggleCategoryList} ref={categoryRef}>
+                    Categorias
+                    <FontAwesomeIcon icon={faChevronDown} />
+                </CategoLink>
+                {isCategoryOpen && (
+                    <CategoryList>
+                        <CategoItem to="/categoria1" onClick={toggleNavBar}>Categoria 1</CategoItem>
+                        <CategoItem to="/categoria2" onClick={toggleNavBar}>Categoria 2</CategoItem>
+                        {/* Adicione mais links para outras categorias aqui */}
+                    </CategoryList>
+                )}
+                <SLink to="/experiencia" onClick={toggleNavBar}>
+                    Carrinho
+                    <FontAwesomeIcon icon={faCartShopping} />
+                </SLink>
+                <SLink to="/projetos" onClick={toggleNavBar}>Projetos</SLink>
+                <SLink to="/contatos" onClick={toggleNavBar}>Contatos</SLink>
+                <button className='nav-btn nav-close-btn' onClick={toggleNavBar}>
+                    <FaTimes />
+                </button>
+            </Nav>
+
+            <button className='nav-btn' onClick={toggleNavBar}>
+                <FaBars />
+            </button>
+
+        </Container>
+    );
 };
 
 export default Header;
