7228ca315b9cb0cb3f3d42b4a62d1b9d1144a521
Reis567 <matheusdosreis9@gmail.com>
2024-04-25 21:21:49 -0300

    Feat : Dropdown style

--- frontend/src/components/Dropdown/Dropdown.tsx
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -1,50 +1,29 @@
 import React from 'react';
-import { DownOutlined, SmileOutlined } from '@ant-design/icons';
+import { DownOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Dropdown, Space } from 'antd';
+import { Dropdown } from 'antd';
+import {SpaceSty} from './Dropdown.styles';
 
 const items: MenuProps['items'] = [
   {
     key: '1',
     label: (
       <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-        1st menu item
+        catego1
       </a>
     ),
   },
-  {
-    key: '2',
-    label: (
-      <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-        2nd menu item (disabled)
-      </a>
-    ),
-    icon: <SmileOutlined />,
-    disabled: true,
-  },
-  {
-    key: '3',
-    label: (
-      <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-        3rd menu item (disabled)
-      </a>
-    ),
-    disabled: true,
-  },
-  {
-    key: '4',
-    danger: true,
-    label: 'a danger item',
-  },
+
+
 ];
 
 const DropPerso: React.FC = () => (
   <Dropdown menu={{ items }}>
     <a onClick={(e) => e.preventDefault()}>
-      <Space>
+      <SpaceSty>
         Categorias
         <DownOutlined />
-      </Space>
+      </SpaceSty>
     </a>
   </Dropdown>
 );
