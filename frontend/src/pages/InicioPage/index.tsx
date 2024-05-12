14450b9b46325214f59d778ba394d81d7015d97b
Reis567 <matheusdosreis9@gmail.com>
2024-05-12 10:49:31 -0300

    Feat : Link envolving prod card

--- frontend/src/pages/InicioPage/index.tsx
+++ frontend/src/pages/InicioPage/index.tsx
@@ -1,35 +1,39 @@
 import React from 'react';
 import { Button } from 'antd';
-import { Container, StyledCard, ProdDiv , SpnSty} from './index.styles.tsx';
+import { Link } from 'react-router-dom'; // Importe o Link do React Router
+import { Container, StyledCard, ProdDiv, SpnSty } from './index.styles.tsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faCartPlus } from '@fortawesome/free-solid-svg-icons';
-import CarouselHome from '../../components/Carousel/index.tsx'
+import CarouselHome from '../../components/Carousel/index.tsx';
 
 const { Meta } = StyledCard;
 
 const Inicio: React.FC = () => {
   return (
     <Container>
-      <CarouselHome/>
+      <CarouselHome />
       <ProdDiv>
-        <StyledCard
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl9su24gdGNlP0Dh4CtT5G4f8KsijhjET9cOymhV8XKg&s" />}
-          actions={[
-            <Button key="favorite" icon={<FontAwesomeIcon icon={faHeart} />} />,
-            <Button key="cart" icon={<FontAwesomeIcon icon={faCartPlus} />} />
-          ]}
-        >
-          <Meta
-            title="Europe Street beat"
-            description="www.instagram.com"
-            style={{marginBottom:10}}
-          />
-          <SpnSty>
-          R$ 99,99
-          </SpnSty>
-        </StyledCard>
+        {/* Envolve o StyledCard com o Link */}
+        <Link to="/produto/1/produto-1-slug"> {/* Passa o ID e o slug do produto */}
+          <StyledCard
+            hoverable
+            style={{ width: 240 }}
+            cover={<img alt="example" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl9su24gdGNlP0Dh4CtT5G4f8KsijhjET9cOymhV8XKg&s" />}
+            actions={[
+              <Button key="favorite" icon={<FontAwesomeIcon icon={faHeart} />} />,
+              <Button key="cart" icon={<FontAwesomeIcon icon={faCartPlus} />} />
+            ]}
+          >
+            <Meta
+              title="Europe Street beat"
+              description="www.instagram.com"
+              style={{ marginBottom: 10 }}
+            />
+            <SpnSty>
+              R$ 99,99
+            </SpnSty>
+          </StyledCard>
+        </Link>
       </ProdDiv>
     </Container>
   );
