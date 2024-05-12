3b073f5364edd23f17cdea997c969a6015625db3
Reis567 <matheusdosreis9@gmail.com>
2024-05-12 10:52:33 -0300

    Feat: styled components of product

--- frontend/src/pages/ProdutoPage/index.styles.tsx
+++ frontend/src/pages/ProdutoPage/index.styles.tsx
@@ -5,3 +5,7 @@ import styled from 'styled-components'
 export const ProdContent = styled.div`
     min-height: 100vh;
 `
+
+export const ProdTitle = styled.h2`
+    font-weight: bolder;
+`
--- frontend/src/pages/ProdutoPage/index.tsx
+++ frontend/src/pages/ProdutoPage/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import {ProdContent,ProdTitle} from  './index.styles.tsx';
 
 const ProdutoDetalhes: React.FC = () => {
   const { id, slug } = useParams<{ id: string; slug: string }>();
 
   return (
-    <div>
-     <h1>Detalhes do Produto</h1>
+    <ProdContent>
+     <ProdTitle>Detalhes do Produto</ProdTitle>
       <p>ID: {id}</p>
       <p>Slug: {slug}</p>
-    </div>
+    </ProdContent>
   );
 };
 
