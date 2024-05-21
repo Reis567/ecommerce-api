d0ee35dd326fe7c44b011321a47405c0736f201f
Reis567 <matheusdosreis9@gmail.com>
2024-05-21 19:29:24 -0300

    Feat : CardCateg

--- frontend/src/pages/CategoriaPage/index.styles.tsx
+++ frontend/src/pages/CategoriaPage/index.styles.tsx
@@ -5,6 +5,14 @@ export const CategoContent = styled.div`
     min-height: 100vh;
 `
 
+
+export const CardCateg = styled.div`
+    min-height: 100vh;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 10px;
+`
+
 export const CatTitle = styled.h2`
     font-weight: bolder;
 `
