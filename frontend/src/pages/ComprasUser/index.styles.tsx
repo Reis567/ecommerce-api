201287027fb2d2b4e6eea61bc827fc0946a17ce5
Reis567 <matheusdosreis9@gmail.com>
2024-05-26 11:21:59 -0300

    Feat : styles

--- frontend/src/pages/ComprasUser/index.styles.tsx
+++ frontend/src/pages/ComprasUser/index.styles.tsx
@@ -1,20 +1,17 @@
 import styled from 'styled-components';
 import { Button } from 'antd';
 
-
 export const ComprasContent = styled.div`
-    min-height: 100vh;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
-
-
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export const Container = styled.div`
   padding: 20px;
-  width: 70%;
+  width: 55%;
 `;
 
 export const Header = styled.h1`
@@ -31,11 +28,17 @@ export const ListaCompras = styled.div`
 
 export const Compra = styled.div`
   display: flex;
-  gap: 20px;
-  align-items: center;
-  padding: 10px;
+  justify-content: space-between;
+  padding: 25px;
   border: 1px solid #ddd;
   border-radius: 10px;
+  height: 300px;
+`;
+
+export const Informacoes = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
 
 export const Imagem = styled.img`
@@ -50,19 +53,22 @@ export const Detalhes = styled.div`
 `;
 
 export const NumeroCompra = styled.h2`
-  font-size: 18px;
+  font-size: 26px;
   margin: 0;
 `;
 
-export const NomeProduto = styled.p`
-  font-size: 16px;
+export const ValorCP = styled.p`
+  font-size: 26px;
   margin: 5px 0;
 `;
 
 export const Status = styled.span`
-  font-size: 14px;
+
   color: #555;
+  font-size: 28px;
+  font-weight: bolder;
 `;
+
 export const NotaFiscalButton = styled(Button)`
   background-color: #389c9c;
   padding: 10px 20px;
@@ -70,8 +76,17 @@ export const NotaFiscalButton = styled(Button)`
   height: auto;
   color: white;
   border: none;
-  display: flex;
-  align-items: center;
-  gap: 10px;
+  font-size: 20px;
+  &:hover {
+    background-color: #2a7b7b;
+  }
+`;
 
+export const Acoes = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-between;
+  gap: 10px;
+  height: 100%;
 `;
--- frontend/src/pages/ComprasUser/index.tsx
+++ frontend/src/pages/ComprasUser/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import {
-Compra,
-Container,
-Detalhes,
-Header,
-Imagem,
-ListaCompras,
-NomeProduto,
-NumeroCompra,
-Status,
-ComprasContent,
-NotaFiscalButton
-
-} from './index.styles'
-
-
+  Compra,
+  Container,
+  Header,
+  Imagem,
+  ListaCompras,
+  ValorCP,
+  NumeroCompra,
+  Status,
+  ComprasContent,
+  NotaFiscalButton,
+  Informacoes,
+  Acoes
+} from './index.styles';
 
 const ComprasUsuario: React.FC = () => {
   const compras = [
@@ -22,38 +20,40 @@ const ComprasUsuario: React.FC = () => {
       numero: '12345',
       produto: 'Produto A',
       imagem: 'https://via.placeholder.com/150',
+      valor: '100,00',
       status: 'Enviado'
     },
     {
       numero: '12346',
       produto: 'Produto B',
       imagem: 'https://via.placeholder.com/150',
+      valor: '150,00',
       status: 'Em Processamento'
     }
   ];
 
   return (
     <ComprasContent>
-        <Container>
-          <Header>Minhas Compras</Header>
-          <ListaCompras>
-            {compras.map((compra, index) => (
-              <Compra key={index}>
+      <Container>
+        <Header>Minhas Compras</Header>
+        <ListaCompras>
+          {compras.map((compra, index) => (
+            <Compra key={index}>
+              <Informacoes>
+                <NumeroCompra>#{compra.numero}</NumeroCompra>
                 <Imagem src={compra.imagem} alt={compra.produto} />
-                <Detalhes>
-                  <NumeroCompra>Compra: {compra.numero}</NumeroCompra>
-                  <NomeProduto>{compra.produto}</NomeProduto>
-                  <Status>{compra.status}</Status>
-                </Detalhes>
-                <NotaFiscalButton>Nota fiscal</NotaFiscalButton>
-              </Compra>
-            ))}
-          </ListaCompras>
-        </Container>
+                <ValorCP>R$ {compra.valor}</ValorCP>
+              </Informacoes>
+              <Acoes>
+                <NotaFiscalButton>Nota Fiscal</NotaFiscalButton>
+                <Status>{compra.status}</Status>
+              </Acoes>
+            </Compra>
+          ))}
+        </ListaCompras>
+      </Container>
     </ComprasContent>
-
   );
 };
 
 export default ComprasUsuario;
-
