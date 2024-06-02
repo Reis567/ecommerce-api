d5bc1788e94555b59ff2a488854f3cefabf7c545
Reis567 <matheusdosreis9@gmail.com>
2024-06-02 17:24:45 -0300

    Feat : user register logic

--- frontend/src/pages/RegistroCliente/index.tsx
+++ frontend/src/pages/RegistroCliente/index.tsx
@@ -6,9 +6,32 @@ import { AuthContainer, AuthForm, AuthTitle, AuthButton } from './index.styles';
 const RegistroCliente: React.FC = () => {
   const navigate = useNavigate();
 
-  const onFinish = (values: any) => {
+  const onFinish = async (values: any) => {
     console.log('Success:', values);
-    navigate('/login-cliente'); // Redireciona para a página de login do cliente
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/auth/user/register/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: values.email,  // Assuming username is email
+          password: values.password,
+          email: values.email,
+          first_name: values.name,
+          last_name: values.last_name
+        }),
+      });
+
+      if (response.ok) {
+        navigate('/login'); // Redireciona para a página de login do cliente
+      } else {
+        const errorData = await response.json();
+        console.error('Failed:', errorData);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -32,10 +55,10 @@ const RegistroCliente: React.FC = () => {
           <Input placeholder="Nome" />
         </Form.Item>
         <Form.Item
-          name="last-name"
+          name="last_name"
           rules={[{ required: true, message: 'Por favor, insira seu sobrenome!' }]}
         >
-          <Input placeholder="Nome" />
+          <Input placeholder="Sobrenome" />
         </Form.Item>
         <Form.Item
           name="email"
