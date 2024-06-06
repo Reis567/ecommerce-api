c0a4f6c2cfed1b7aa251b472b79e55d955083c1f
Reis567 <matheusdosreis9@gmail.com>
2024-06-06 19:29:01 -0300

    Feat : photos of product

--- main/models.py
+++ main/models.py
@@ -39,6 +39,8 @@ class ProductCondition(models.Model):
     def __str__(self):
         return self.get_condition_display()
 
+
+
 class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL, null=True)
     condition=models.ForeignKey(ProductCondition, on_delete=models.SET_NULL, null=True)
@@ -46,13 +48,19 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     title = models.CharField(max_length=255, unique=True)
     detail = models.TextField(blank=True, null=True)
+    photo_product1 = models.ImageField(upload_to='products/', blank=True, null=True)
+    photo_product2 = models.ImageField(upload_to='products/', blank=True, null=True)
+    photo_product3 = models.ImageField(upload_to='products/', blank=True, null=True)
+    photo_product4 = models.ImageField(upload_to='products/', blank=True, null=True)
+    photo_product5 = models.ImageField(upload_to='products/', blank=True, null=True)
+
 
     created_at = models.DateTimeField(default=timezone.now) 
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title} - ${self.price}"
-    
+
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     mobile = models.PositiveBigIntegerField()
