a00a79de9a8d7a1da761ecfba0f96f67f3716146
Reis567 <matheusdosreis9@gmail.com>
2024-06-08 22:59:25 -0300

    Feat : initial logic of header refact

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { NavContainer, SearchContainer, HeaderCont, Nav, NavTitle, SLink, NavRight } from './Header.styles.tsx';
-import SearchBar from '../SearchBar/SearchBar.tsx'; // Importe o componente de campo de pesquisa aqui
+import SearchBar from '../SearchBar/SearchBar.tsx';
 import DropPerso from '../Dropdown/Dropdown.tsx';
 import DropdownProfile from '../DropdownProfile/DropdownProfile.tsx';
 import { Link } from 'react-router-dom';
@@ -10,9 +10,39 @@ import { faCartShopping, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Header: React.FC = () => {
     const [isNavOpen, setIsNavOpen] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [userData, setUserData] = useState<any>(null);
 
     const toggleNavBar = () => {
         setIsNavOpen(!isNavOpen);
+    };
+
+    useEffect(() => {
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            fetchUserData(token);
+        }
+    }, []);
+
+    const fetchUserData = async (token: string) => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/api/v1/user/', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            setUserData(data);
+            setIsAuthenticated(true);
+        } catch (error) {
+            console.error('Failed to fetch user data:', error);
+            setIsAuthenticated(false);
+        }
     };
 
     return (
@@ -28,7 +58,7 @@ const Header: React.FC = () => {
 
             <NavContainer>
                 <Nav isOpen={isNavOpen} as="div">
-                    <DropPerso/>
+                    <DropPerso />
                     <SLink to="/ofertas" onClick={toggleNavBar}>Ofertas</SLink>
                     <SLink to="/supermercado" onClick={toggleNavBar}>Supermercado</SLink>
                     <SLink to="/moda" onClick={toggleNavBar}>Moda</SLink>
@@ -45,30 +75,36 @@ const Header: React.FC = () => {
                     <FaBars />
                 </button>
                 <NavRight>
-                    <DropdownProfile/>
-                    <SLink to="/perfil/favoritos">
-                        <FontAwesomeIcon icon={faHeart} />
-                    </SLink>
-                    <SLink to="/perfil/carrinho">
-                        <FontAwesomeIcon icon={faCartShopping} />
-                    </SLink>
-                    <SLink to="/perfil/compras">
-                        Compras
-                    </SLink>
-                    {/*
-                    <SLink to="/login">
-                        Login
-                    </SLink>
-                    <SLink to="/register">
-                        Registrar
-                    </SLink>
-                    <SLink to="/vendedor/login">
-                        Login Vendedor
-                    </SLink>
-                    <SLink to="/vendedor/register">
-                        Registrar Vendedor
-                    </SLink>
-                    */}
+                    {isAuthenticated ? (
+                        <>
+                            <DropdownProfile />
+                            <span>{userData?.username}</span>
+                            <SLink to="/perfil/favoritos">
+                                <FontAwesomeIcon icon={faHeart} />
+                            </SLink>
+                            <SLink to="/perfil/carrinho">
+                                <FontAwesomeIcon icon={faCartShopping} />
+                            </SLink>
+                            <SLink to="/perfil/compras">
+                                Compras
+                            </SLink>
+                        </>
+                    ) : (
+                        <>
+                            <SLink to="/login">
+                                Login
+                            </SLink>
+                            <SLink to="/register">
+                                Registrar
+                            </SLink>
+                            <SLink to="/vendedor/login">
+                                Login Vendedor
+                            </SLink>
+                            <SLink to="/vendedor/register">
+                                Registrar Vendedor
+                            </SLink>
+                        </>
+                    )}
                 </NavRight>
             </NavContainer>
         </HeaderCont>
