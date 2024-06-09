8ec829fcbe21288f25e24fc16169098691bff0c8
Reis567 <matheusdosreis9@gmail.com>
2024-06-08 23:13:04 -0300

    Feat : user route

--- Auth/urls.py
+++ Auth/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', views.CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('user/register/', views.UserRegistrationView.as_view(), name='user-registration'),
+    path('user/', views.UserDetailView.as_view(), name='user-detail'),
 ]
