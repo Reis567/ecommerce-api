f4fa7bc21db4ab86e7a2d25c61a64d9b50ba21b6
Reis567 <matheusdosreis9@gmail.com>
2024-07-05 08:55:55 -0300

    Fix : footer

--- frontend/src/components/Footer/Footer.style.tsx
+++ frontend/src/components/Footer/Footer.style.tsx
@@ -1,9 +1,8 @@
-
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const FooterContainer = styled.footer`
-display:flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
   width: 100%;
   background-color: #2a2a2a;
   color: #fff;
@@ -11,17 +10,26 @@ flex-direction: column;
   text-align: center;
 `;
 
-export const FooterItems = styled.footer`
-display:flex;
+export const FooterItems = styled.div`
+  display: flex;
   width: 100%;
+  justify-content: space-around;
 `;
 
-
 export const FooterItem = styled.div`
-  width: 33.33%;
-  display: inline-block;
+  width: 30%;
+  text-align: center;
+`;
+
+export const FooterIcon = styled.div`
+  margin-bottom: 10px;
+`;
+
+export const FooterText = styled.div`
+  font-size: 20px;
 `;
 
 export const Copyright = styled.div`
   margin-top: 20px;
+  font-size: 19px;
 `;
--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-
-import {FooterContainer , FooterItem, Copyright,FooterItems} from './Footer.style'
-
+import { FaCreditCard, FaBox, FaBriefcase } from 'react-icons/fa'; // Adicionando ícones
+import { FooterContainer, FooterItem, FooterIcon, FooterText, Copyright, FooterItems } from './Footer.style';
 
 const Footer: React.FC = () => {
     return (
       <FooterContainer>
         <FooterItems>
-
-            <FooterItem>Item 1</FooterItem>
-            <FooterItem>Item 2</FooterItem>
-            <FooterItem>Item 3</FooterItem>
+            <FooterItem>
+                <FooterIcon><FaCreditCard size={60} /></FooterIcon>
+                <FooterText>Formas de pagamento aceitas: Cartões de crédito, débito e PIX.</FooterText>
+            </FooterItem>
+            <FooterItem>
+                <FooterIcon><FaBox size={60} /></FooterIcon>
+                <FooterText>Entrega rápida e segura para todo o país.</FooterText>
+            </FooterItem>
+            <FooterItem>
+                <FooterIcon><FaBriefcase size={60} /></FooterIcon>
+                <FooterText>Trabalhe conosco e tenha oportunidades de crescimento.</FooterText>
+            </FooterItem>
         </FooterItems>
-        <Copyright>&copy; NegocioFechado 2024</Copyright>
+        <Copyright>
+          &copy; NegocioFechado 2024 | CNPJ: 00.000.000/0000-00 | Endereço: Rua Fictícia, 123, Cidade Exemplo, País
+        </Copyright>
       </FooterContainer>
     );
-  };
- export default Footer;
- 
+};
+
+export default Footer;
