def5e4ec956be0a47b9d06812de92e33b41ab718
Reis567 <matheusdosreis9@gmail.com>
2024-07-11 09:13:12 -0300

    Feat : SearchBar

--- frontend/src/components/SearchBar/SearchBar.tsx
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { SearchOutlined } from '@ant-design/icons';
-import {InputSty} from './Searchbar.styles'
+import { InputSty } from './Searchbar.styles';
+import { useNavigate } from 'react-router-dom';
 
+const SearchBar: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate();
 
-const SearchBar: React.FC = () => (
-  <InputSty
-    size="large"
-    placeholder="Pesquisar produtos"
-    prefix={<SearchOutlined />}
-  />
-);
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    if (searchQuery.trim()) {
+      navigate(`/search-results?query=${encodeURIComponent(searchQuery.trim())}`);
+    }
+  };
+
+  return (
+    <InputSty
+      size="large"
+      placeholder="Pesquisar produtos"
+      prefix={<SearchOutlined />}
+      value={searchQuery}
+      onChange={handleSearch}
+      onPressEnter={handleSearchSubmit}
+    />
+  );
+};
 
 export default SearchBar;
