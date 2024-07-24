47b0b3a330fc53f28d2678268e74afd74daf9327
Reis567 <matheusdosreis9@gmail.com>
2024-07-24 08:35:47 -0300

    Feat : cart models

--- main/models.py
+++ main/models.py
@@ -167,3 +167,13 @@ class ProductComment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.user.username} on {self.product.title}'
+    
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
